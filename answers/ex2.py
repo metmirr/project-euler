d96e89289f5d94bc97e5c810d5f566f93ec65806
metmirr <metin7087@gmail.com>
2018-06-27 09:33:17 +0300

    Added Problem 2 solution

---
+++ answers/ex2.py
@@ -0,0 +1,31 @@
+"""
+    Problem 2:
+    By considering the terms in the Fibonacci sequence whose values do not
+    exceed four million, find the sum of the even-valued terms.
+"""
+
+
+class Fib:
+    def __init__(self):
+        self.curr = 0
+        self.next = 1
+
+    def __next__(self):
+        self.new_next = self.curr + self.next
+        self.curr = self.next
+        self.next = self.new_next
+        if self.curr > 4000000:
+            raise StopIteration
+        return self.curr
+
+    def __iter__(self):
+        return self
+
+
+answer = 0
+
+for i in Fib():
+    if i % 2 == 0:
+        answer += i
+
+print('answer:', answer)
